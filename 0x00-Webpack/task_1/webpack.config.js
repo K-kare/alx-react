5af12491fad51647a6e1b0dac202ff2518cb28b6
K-kare <kamunawangari@gmail.com>
2024-08-20 18:52:54 +0000

    Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,7 +1,10 @@
-let count = 0;
-function updateCounter() {
-    count += 1;
-    document.getElementById('count').textContent = `${count} clicks on the button`;
-}
-const debouncedUpdateCounter = _.debounce(updateCounter, 300); 
-document.getElementById('myButton').addEventListener('click', debouncedUpdateCounter);
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
